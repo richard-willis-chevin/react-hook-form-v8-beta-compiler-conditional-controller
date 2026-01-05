17fad598ebb4cf6a277bdc2776c53884ac67d5ed
Richard Willis <richard.willis@chevinfleet.com>
2026-01-05 15:07:08 +0000

    Update

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ When using `react-hook-form` with:
 
 - A `resolver` for validation
 - `"react-hook-form": "^8.0.0-beta.0"`
-- react-compiler
 - conditionally render a `Controller`
 
 You get: `Cannot update a component (Controller) while rendering a different component (Controller)`
--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          [
-            'babel-plugin-react-compiler',
-            {
-              sources: (_filename: string) => {
-                return true;
-              },
-            },
-          ],
-        ],
-      },
-    }),
+    react(),
   ],
 });
